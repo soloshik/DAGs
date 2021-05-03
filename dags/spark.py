2dc2ac3f90ffb38f5dfd1ddfa9f055647c9e006a
PAmAN <33486320+soloshik@users.noreply.github.com>
2021-05-03 20:34:52 +0300

    Update spark.py

--- dags/spark.py
+++ dags/spark.py
@@ -23,6 +23,6 @@ dag = DAG(dag_id='spark_job_dag',
           schedule_interval='0,10,20,30,40,50 * * * *')
 
 cmd = """
-/opt/spark/work-dir/bin/spark-submit --master k8s://https://aksdns-ff976585.hcp.westeurope.azmk8s.io:443 --deploy-mode cluster --name spark-pi --class org.apache.spark.examples.SparkPi --conf spark.executor.instances=3 --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark --conf spark.kubernetes.container.image=soloshik/spark:v2 local:///opt/spark/work-dir/SparkPi-assembly-0.1.0-SNAPSHOT.jar 
+/opt/spark/bin/spark-submit --master k8s://https://aksdns-ff976585.hcp.westeurope.azmk8s.io:443 --deploy-mode cluster --name spark-pi --class org.apache.spark.examples.SparkPi --conf spark.executor.instances=3 --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark --conf spark.kubernetes.container.image=soloshik/spark:v2 local:///opt/spark/work-dir/SparkPi-assembly-0.1.0-SNAPSHOT.jar 
 """
 t = BashOperator(task_id='Spark_datamodel',bash_command=cmd,dag=dag)
